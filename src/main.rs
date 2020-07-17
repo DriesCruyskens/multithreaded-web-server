4850367f8e572c7d870a1c054cf26a3a7cd68f65
Dries Cruyskens <dries.cruyskens@gmail.com>
2020-07-17 10:40:15 +0200

    Added 404 page.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
-use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -21,10 +21,18 @@ fn handle_connection(stream: &mut TcpStream) {
     println!("{}", String::from_utf8_lossy(&buffer));
     println!("request From: {}", stream.peer_addr().unwrap().to_string());
 
-    let contents = fs::read_to_string("index.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!("HTTP/1.1 200 OK\r\nContent-length: {}\r\n\r\n{}", contents.len(), contents);
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-    stream.write( response.as_bytes()).unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
