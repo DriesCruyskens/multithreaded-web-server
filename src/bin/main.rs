92bed967ce42e95cdfbda29b899ddfb6884e5f75
Dries Cruyskens <dries.cruyskens@gmail.com>
2020-07-17 10:23:04 +0200

    Moved binary file to bin folder. Added workers.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,30 +1,36 @@
+use multithreaded_web_server::ThreadPool;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        handle_connection(&mut stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
-fn handle_connection(stream: &mut TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    println!("{}", String::from_utf8_lossy(&buffer));
-    println!("request From: {}", stream.peer_addr().unwrap().to_string());
-
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "index.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
